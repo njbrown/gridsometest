81e4438c89c2f031e3f14f0ea85c0d975c947ae3
Nicolas Brown <nclsbrwn@gmail.com>
2019-10-16 16:07:27 -0500

    initial commit

---
+++ src/pages/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>Contact Us</h1>
+    <p>Dont you fucking dare try to contact us at all >:(</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Contact Us"
+  }
+};
+</script>
